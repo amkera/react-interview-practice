906304e77b2935418664e46cc4adc7393bb0ad0e
amkera <amber_kerawala@berkeley.edu>
2022-08-10 11:25:05 -0400

    Add pagination

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react"
 import axios from "axios"
 const URL = "https://randomuser.me/api"
 
-const fetchRandomData = () => {
+const fetchRandomData = (pageNumber) => {
   return axios
-    .get(URL)
+    .get(`${URL}?page=${pageNumber}`)
     .then(({ data }) => {
       return data
     })
@@ -22,18 +22,36 @@ const getFullUserName = (userInfo) => {
 
 const App = () => {
   const [randomUserDataJSON, setRandomUserDataJSON] = React.useState("")
+  const [nextPageNumber, setNextPageNumber] = React.useState(1)
   const [userInfos, setUserInfos] = React.useState([])
 
-  useEffect(() => {
-    fetchRandomData().then((randomData) => {
-      setRandomUserDataJSON(randomData || "N/A")
+  // useEffect(() => {
+  //   fetchRandomData().then((randomData) => {
+  //     setRandomUserDataJSON(randomData || "N/A")
 
-      setUserInfos(randomData.results)
+  //     setUserInfos(randomData.results)
+  //     setNextPageNumber(randomData.info.page + 1)
+  //   })
+  // }, [])
+
+  const fetchNextUser = () => {
+    fetchRandomData().then((randomData) => {
+      if (randomData === undefined) return
+      setRandomUserDataJSON(randomData || "")
+      const newUserInfos = [...userInfos, ...randomData.results]
+
+      setUserInfos(newUserInfos)
+      setNextPageNumber(randomData.info.page + 1)
     })
+  }
+
+  useEffect(() => {
+    fetchNextUser()
   }, [])
 
   return (
     <div>
+      <button onClick={() => fetchNextUser()}>Fetch Next User</button>
       <p>hi</p>
       {userInfos.map((userInfo, idx) => (
         <div key={idx}>
